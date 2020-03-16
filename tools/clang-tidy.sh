bf49e8785d34a4b18268f93381c5887fbf4cecf5
Karol Baryła <lorak.mmk@gmail.com>
2020-03-16 02:28:08 +0100

    Initial clang-tidy setup

---
+++ tools/clang-tidy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# https://stackoverflow.com/a/16349776
+cd "${0%/*}"
+
+./run-clang-tidy.py \
+  -p ../build \
+  -checks="-*,bugprone-*,cert-*,cppcoreguidelines-*,clang-analyzer-*,misc-*,modernize-*,performance-*,portability-*,readability-*,-modernize-use-trailing-return-type" \
+  -quiet \

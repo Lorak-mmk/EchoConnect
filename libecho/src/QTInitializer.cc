ce85696aa7dc566048a5a3423c75d311fce04c11
Karol Baryła <lorak.mmk@gmail.com>
2020-03-19 14:19:37 +0100

    Destroy QTCoreApplication

--- libecho/src/QTInitializer.cc
+++ libecho/src/QTInitializer.cc
@@ -8,6 +8,10 @@ QTInitializer::QTInitializer(int a_argc, char **a_argv) {
     }
 }
 
+QTInitializer::~QTInitializer() {
+    delete app;
+}
+
 void echo::initEcho(int a_argc, char **a_argv) {
     static QTInitializer init(a_argc, a_argv);
 }
--- libecho/src/QTInitializer.h
+++ libecho/src/QTInitializer.h
@@ -7,6 +7,7 @@ class QTInitializer : public QObject {
     Q_OBJECT
 public:
     QTInitializer(int argc, char **argv);
+    ~QTInitializer();
 
 private:
     QCoreApplication *app;

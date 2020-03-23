d72873f4db6de64c39f4b88ae648be04f730a847
Jagoda Kamińska <jk406179@students.mimuw.edu.pl>
2020-03-23 12:31:15 +0100

    WIP: data to send encoding

--- libecho/src/echo.cpp
+++ libecho/src/echo.cpp
@@ -4,25 +4,58 @@
 #include "AudioOutput.h"
 #include "QTInitializer.h"
 
-void echo::send(const std::vector<uint8_t> &buffer) {
-    static AudioOutput audio(AudioFormatFactory::getDefaultOutputFormat());
+#include <cmath>
+#include <QCoreApplication>
+#include <QDebug>
 
-    /* Example transmission */
-    const size_t atOnce = 260;  // (bitrate / notify_interval) + eps;
-    // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
-    audio.enqueueData(reinterpret_cast<const char *>(buffer.data()), std::min(buffer.size(), atOnce));
-
-    for (int i = atOnce; i < buffer.size(); i += atOnce) {
-        // Push some bytes to sound output
-        // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast, cppcoreguidelines-pro-bounds-pointer-arithmetic)
-        audio.enqueueData(reinterpret_cast<const char *>(buffer.data() + i), std::min(atOnce, buffer.size() - i));
-        auto status = audio.getStreamStatus();
-        qDebug() << "Status: " << status.first << " " << status.second;
-        audio.waitForTick();
+namespace {
+    const int loRate = 400;
+    const int hiRate = 500;
+    const double windowTime = 0.1;
+    using SampleType = int16_t;
+    
+    SampleType audio_wave(int frequency, int time, const QAudioFormat& format) {
+        return sin(frequency * time / format.sampleRate()) * (1 << (format.sampleSize() - 1));
     }
-    /* End example */
+    
+    std::vector<SampleType> encode_byte(char byte, const QAudioFormat& format){
+        std::vector<SampleType> encoded;
+        for (int i = 0; i < 8; i++) {
+            int freq = (byte >> i) & 1 ? hiRate : loRate;
+            for (int t = 0; t < format.sampleRate() * windowTime; t++) {
+                encoded.emplace_back(audio_wave(freq, t, format));
+            }
+        }
+        return encoded;
+    }
+    
+    std::vector<SampleType> encode_data(const std::vector<char>& data, const QAudioFormat& format) {
+        std::vector<SampleType> encoded;
+        for (const auto byte : data) {
+            auto encoded_byte = encode_byte(byte, format);
+            encoded.insert(encoded.end(), encoded_byte.begin(), encoded_byte.end());
+        }
+        return encoded;
+    }
+    
+    void play(AudioOutput& audio, std::vector<SampleType>& buffer) {
+        const size_t atOnce = 260; // (bitrate / notify_interval) + eps; - TODO: wyjaśnić
+        
+        audio.enqueueData(reinterpret_cast<char *> (buffer.data()), std::min(buffer.size(), atOnce));
+        for(int i = atOnce; i <  buffer.size(); i += atOnce) {
+            audio.enqueueData(reinterpret_cast<char *> (buffer.data() + i), std::min(atOnce, buffer.size() - i));
+            audio.waitForTick();
+        }
+        
+        audio.waitForState(QAudio::State::IdleState);
+    }
+}
 
-    audio.waitForState(QAudio::State::IdleState);
+void echo::send(const std::vector<char>& buffer) {
+    static auto format = AudioFormatFactory::getDefaultOutputFormat();
+    static AudioOutput audio(format);
+    auto to_play = encode_data(buffer, format);
+    play(audio, to_play);
 }
 
 std::vector<uint8_t> echo::receive() {

2ce3213ee553ef4065ae6695bd005513b2b6a411
Karol Baryła <lorak.mmk@gmail.com>
2020-04-12 13:50:58 +0200

    CI: clang-tidy

--- tools/clang-tidy.sh
+++ tools/clang-tidy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # https://stackoverflow.com/a/16349776
-cd "${0%/*}"
+cd "${0%/*}" || exit
 
-./run-clang-tidy.py \
-  -p ../build \
-  -checks="-*,bugprone-*,cert-*,cppcoreguidelines-*,clang-analyzer-*,misc-*,modernize-*,performance-*,portability-*,readability-*,-modernize-use-trailing-return-type,-cppcoreguidelines-owning-memory" \
-  -quiet \
+CHECK_DIRS=("../libecho/src" "../libecho/include")
+CHECKS="-*,bugprone-*,cert-*,cppcoreguidelines-*,clang-analyzer-*,misc-*,modernize-*,performance-*,portability-*,readability-*,-modernize-use-trailing-return-type,-cppcoreguidelines-owning-memory,-cppcoreguidelines-non-private-member-variables-in-classes,-misc-non-private-member-variables-in-classes"
+
+find "${CHECK_DIRS[@]}" -type f \( -name '*\.cpp' -o -name '*\.cc' \) -print0 |  \
+xargs --null clang-tidy -header-filter='.*' -checks="$CHECKS" -p=../build -quiet --warnings-as-errors="$CHECKS"

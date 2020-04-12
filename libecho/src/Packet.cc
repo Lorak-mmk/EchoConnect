2afd64170ac3864594ca680a4419d29ff6bd1153
Jagoda Kamińska <jk406179@students.mimuw.edu.pl>
2020-04-12 15:41:16 +0200

    implemented packets (Packet.*)

---
+++ libecho/src/Packet.cc
@@ -0,0 +1,112 @@
+#include "Packet.h"
+
+// #define crcpp_uint32
+#include "CRC.h"
+#include <arpa/inet.h>
+
+Packet Packet::loadHeaderFromBytes(const std::vector<uint8_t> &bytes) {
+    Packet p;
+    
+    if (bytes.size() != HEADER_SIZE) {
+        throw IncorrectFormat();
+    }
+
+    p.flags = ntohs(((uint16_t *)bytes.data())[0] );
+    p.size = ntohs(((uint16_t *)bytes.data())[1]);
+    p.number = ntohs(((uint16_t *)bytes.data())[2]);
+    
+    return p;
+}
+
+Packet &Packet::loadDataFromBytes(const std::vector<uint8_t> &bytes) {
+    if (bytes.size() != size) {
+        throw IncorrectFormat();
+    }
+
+    data = bytes;
+
+    return *this;
+}
+
+Packet &Packet::loadCRCFromBytes(const std::vector<uint8_t> &bytes) {
+    if (bytes.size() != CRC_SIZE) {
+        throw IncorrectFormat();
+    }
+
+    crc32 = ntohl(((uint32_t *)bytes.data())[0]);
+    if (crc32 != calculateCRC()) {
+        throw IncorrectCRC();
+    }
+
+    return *this;
+}
+
+std::vector<uint8_t> Packet::toBytes() {
+    std::vector<uint8_t> bytes;
+    bytes.reserve(FRAME_SIZE + size);
+    bytes.push_back(htons(flags));
+    bytes.push_back(htons(size));
+    bytes.push_back(htons(number));
+    bytes.insert(bytes.end(), data.begin(), data.end());
+    bytes.push_back(htonl(crc32));
+    return bytes;
+}
+
+bool Packet::isSet(Flag f) {
+    return flags & f;
+}
+
+uint16_t Packet::getSize() {
+    return size;
+}
+
+uint16_t Packet::getNumber() {
+    return number;
+}
+
+const std::vector<uint8_t> &Packet::getData() {
+    return data;
+}
+
+uint32_t Packet::calculateCRC() {
+    uint32_t result;
+    result = CRC::Calculate(&flags, sizeof(flags), CRC::CRC_32());
+    result = CRC::Calculate(&size, sizeof(size), CRC::CRC_32(), result);
+    result = CRC::Calculate(&number, sizeof(number), CRC::CRC_32(), result);
+    result = CRC::Calculate(data.data(), data.size(), CRC::CRC_32(), result);
+    return result;
+}
+
+void Packet::updateCRC() {
+    crc32 = calculateCRC();
+}
+
+PacketBuilder &PacketBuilder::setData(const std::vector<uint8_t> &data) {
+    if (data.size() > MAX_DATA_SIZE) {
+        throw Packet::OversizedData();
+    }
+
+    pckt.data = data;
+    pckt.size = data.size();
+    return *this;
+}
+
+PacketBuilder &PacketBuilder::setFlag(Flag f) {
+    pckt.flags |= f;
+    return *this;
+}
+
+PacketBuilder &PacketBuilder::setNumber(uint16_t num) {
+    pckt.number = num;
+    return *this;
+}
+
+Packet PacketBuilder::getPacket() {
+    pckt.updateCRC();
+    return pckt;
+}
+
+std::vector<uint8_t> PacketBuilder::getBytes() {
+    pckt.updateCRC();
+    return pckt.toBytes();
+}
---
+++ libecho/src/Packet.h
@@ -0,0 +1,79 @@
+#ifndef ECHOCONNECT_PACKET_H
+#define ECHOCONNECT_PACKET_H
+
+#include <vector>
+#include <limits>
+#include <cstdint>
+#include <exception>
+
+static constexpr size_t HEADER_SIZE = 6;
+static constexpr size_t CRC_SIZE = 4;
+static constexpr size_t FRAME_SIZE = HEADER_SIZE + CRC_SIZE;
+static constexpr size_t MAX_DATA_SIZE = std::numeric_limits<uint16_t>::max();
+
+enum Flag : uint16_t {
+    SYN = 1 << 0,  // Synchronization
+    ACK = 1 << 1,  // Acknowledgement
+    DMD = 1 << 2,  // Demand resend
+    FIN = 1 << 3,  // Finish
+    RST = 1 << 4,  // Reset
+    LPC = 1 << 5   // Last packet
+};
+
+class Packet {
+public:
+    static Packet loadHeaderFromBytes(const std::vector<uint8_t> &bytes);
+    Packet &loadDataFromBytes(const std::vector<uint8_t> &bytes);
+    Packet &loadCRCFromBytes(const std::vector<uint8_t> &bytes);
+    std::vector<uint8_t> toBytes();
+    bool isSet(Flag f);
+    uint16_t getSize();
+    uint16_t getNumber();
+    const std::vector<uint8_t> &getData();
+
+private:
+    Packet() = default;
+    uint32_t calculateCRC();
+    void updateCRC();
+    uint16_t flags;
+    uint16_t size;
+    uint16_t number;
+    std::vector<uint8_t> data;
+    uint32_t crc32;
+
+    friend class PacketBuilder;
+
+    class IncorrectCRC : public std::exception {
+        const char *what() const throw() {
+            return "Incorrect package CRC";
+        }
+    };
+
+    class IncorrectFormat : public std::exception {
+        const char *what() const throw() {
+            return "Incorrect package format";
+        }
+    };
+
+    class OversizedData : public std::exception {
+        const char *what() const throw() {
+            return "Too large data";
+        }
+    };
+};
+
+class PacketBuilder {
+public:
+    PacketBuilder() = default;
+    PacketBuilder(const PacketBuilder &pb) = delete;
+    PacketBuilder &setData(const std::vector<uint8_t> &data);
+    PacketBuilder &setFlag(Flag f);
+    PacketBuilder &setNumber(uint16_t num);
+    Packet getPacket();
+    std::vector<uint8_t> getBytes();
+
+private:
+    Packet pckt;
+};
+
+#endif  // ECHOCONNECT_PACKET_H

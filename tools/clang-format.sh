148b70a078952f4c4e7ea7492c95b9bbd14c8c43
Jagoda Kamińska <39906660+Riyria-was-taken@users.noreply.github.com>
2020-04-13 12:16:51 +0200

    packets implementation  and clang-format fix
    * added external library CRCpp, updated README.md
    
    * implemented packets (Packet.*)
    
    * fixed clang-tidy warnings
    
    * resolved PR comments
    
    * fixed clang-format not to change external files
    
    Note: Documentation and tests in another PR.

--- tools/clang-format.sh
+++ tools/clang-format.sh
@@ -3,7 +3,7 @@
 # https://stackoverflow.com/a/16349776
 cd "${0%/*}"
 
-FILES=$(find ../libecho -type f \( -name '*\.cpp' -o -name '*\.cc' -o -name '*\.h' \))
+FILES=$(find ../libecho -type f \( -name '*\.cpp' -o -name '*\.cc' -o -name '*\.h' \) -not -path "../libecho/external/*" )
 
 if hash clang-format-8 2>/dev/null; then
     CLANG_FORMAT="clang-format-8"

a98e63d4eb78e8bd5b550fc337338d9efd658eec
Karol Baryła <lorak.mmk@gmail.com>
2020-05-13 18:02:29 +0200

    [WIP] Basic CMake structure for DemoApp (#82)
    * Basic CMake structure for DemoApp

---
+++ DemoApp/src/main.cc
@@ -0,0 +1,46 @@
+#include "Echo.h"
+
+#define N 300
+
+using namespace std::chrono_literals;
+
+uint8_t out[N];
+uint8_t in[N];
+
+int main(int argc, char **argv) {
+    Echo::initEcho(argc, argv);
+
+    if (argc < 4) {
+        printf(
+                "usage: sendrecv <win_size> <freq> <lim> [dbg]\n"
+                "The dbg parameter is for reporting parts of the waveform\n"
+                "that were received incorrectly. If you wish to enable it,\n"
+                "just put any text there.\n");
+        return 1;
+    }
+    int win_size = atoi(argv[1]);
+    int freq = atoi(argv[2]);
+    int lim = atoi(argv[3]);
+
+    auto connection = EchoRawConnection::getBitEchoRawConnection(win_size, freq, freq, lim);
+
+    connection->send(out, 40);
+    for (int i = 0; i < N; i++)
+        out[i] = i;
+    connection->sendStart();
+    connection->send(out, N);
+
+    connection->receiveStart(30s);
+    connection->receive(in, N);
+
+    int wrong = 0;
+    for (int i = 0; i < N; i++) {
+        if (in[i] == out[i]) {
+            printf("%02hhx ", in[i]);
+        } else {
+            printf("\x1b[31m%02hhx\x1b[m ", in[i]);
+            wrong++;
+        }
+    }
+    printf("\nwrong: %d\n", wrong);
+}

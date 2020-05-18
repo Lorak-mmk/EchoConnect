613d06b8cdc71890b75981ce23b40049577792b3
Jagoda Kamińska <39906660+Riyria-was-taken@users.noreply.github.com>
2020-05-18 18:03:19 +0200

    fixes (#94)
    * Changed IAction, IView to AAction, AView
    
    * Console.*, Action: Help, more functions in Utils, fixes
    - added functions wrapping ANSII escape codes (Console)
    - added Help action
    - added new functionalities to Utils: waiting for enter press, reading a value of given type, showing error message, printing view title
    - visual fixes when showing menu, go to value checks

---
+++ DemoApp/src/AAction.h
@@ -0,0 +1,24 @@
+#ifndef DEMOAPP_IACTION_H
+#define DEMOAPP_IACTION_H
+
+#include <string>
+#include <vector>
+
+#include "AView.h"
+#include "Argument.h"
+
+class AAction : public AView {
+public:
+    AAction(std::string name, std::string title) : AView(name, title) {}
+
+    virtual ~AAction() = default;
+
+    ViewPtr execute() override;
+
+protected:
+    std::vector<Argument> arguments;
+    virtual void setDefaults() = 0;
+    virtual ViewPtr runAction() = 0;
+};
+
+#endif  // DEMOAPP_IACTION_H
